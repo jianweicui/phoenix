7e82898fd7467e1a9da2f1a12ea259c06719e7a9
Jesse Yates <jesse.k.yates@gmail.com>
2013-07-01 00:45:05 -0700

    Adding test for deletes.
    TODO:
     - causing Concurrent Lock Access Exceptions. (sad face)

--- contrib/hbase-index/index-core/src/test/java/com/salesforce/hbase/index/builder/covered/TestEndToEndCoveredIndexing.java
+++ contrib/hbase-index/index-core/src/test/java/com/salesforce/hbase/index/builder/covered/TestEndToEndCoveredIndexing.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
@@ -72,9 +75,6 @@ public class TestEndToEndCoveredIndexing {
    */
   @Test
   public void testSimpleTimestampedUpdates() throws Exception {
-    byte[] indexed_qualifer = Bytes.toBytes("indexed_qual");
-    byte[] regular_qualifer = Bytes.toBytes("reg_qual");
-
     //setup the index 
     CoveredColumnIndexSpecifierBuilder builder = new CoveredColumnIndexSpecifierBuilder();
     builder.addIndexGroup(fam1);
@@ -107,12 +107,6 @@ public class TestEndToEndCoveredIndexing {
 
     // read the index for the expected values
     HTable index1 = new HTable(UTIL.getConfiguration(), INDEX_TABLE);
-    Scan s = new Scan(value1);
-    List<KeyValue> received = new ArrayList<KeyValue>();
-    ResultScanner scanner = index1.getScanner(s);
-    for (Result r : scanner) {
-      received.addAll(r.list());
-    }
 
     // build the expected kvs
     List<Pair<byte[], CoveredColumn>> pairs = new ArrayList<Pair<byte[], CoveredColumn>>();
@@ -120,13 +114,11 @@ public class TestEndToEndCoveredIndexing {
     pairs.add(new Pair<byte[], CoveredColumn>(EMPTY_BYTES, col2));
     List<KeyValue> expected = CoveredColumnIndexCodec.getIndexKeyValueForTesting(row1, ts, pairs);
 
-    assertEquals("Didn't get the expected kvs from the index table!", expected, received);
+    // verify that the index matches
+    verifyIndexTableAtTimestamp(index1, expected, ts, value1);
 
     // cleanup
-    primary.close();
-    index1.close();
-    UTIL.deleteTable(Bytes.toBytes(INDEX_TABLE));
-    UTIL.deleteTable(Bytes.toBytes(indexedTableName));
+    closeAndCleanupTables(primary, index1);
   }
 
   /**
@@ -135,9 +127,6 @@ public class TestEndToEndCoveredIndexing {
    */
   @Test
   public void testMultipleTimestampsInSinglePut() throws Exception {
-    byte[] indexed_qualifer = Bytes.toBytes("indexed_qual");
-    byte[] regular_qualifer = Bytes.toBytes("reg_qual");
-
     // setup the index
     CoveredColumnIndexSpecifierBuilder builder = new CoveredColumnIndexSpecifierBuilder();
     builder.addIndexGroup(fam1);
@@ -192,10 +181,7 @@ public class TestEndToEndCoveredIndexing {
     verifyIndexTableAtTimestamp(index1, expected, ts2, value1);
 
     // cleanup
-    primary.close();
-    index1.close();
-    UTIL.deleteTable(Bytes.toBytes(INDEX_TABLE));
-    UTIL.deleteTable(Bytes.toBytes(indexedTableName));
+    closeAndCleanupTables(primary, index1);
   }
 
   /**
@@ -256,17 +242,148 @@ public class TestEndToEndCoveredIndexing {
     verifyIndexTableAtTimestamp(index2, expected, ts, value3);
 
     // cleanup
-    primary.close();
-    index1.close();
-    index2.close();
-    UTIL.deleteTable(Bytes.toBytes(INDEX_TABLE));
-    UTIL.deleteTable(Bytes.toBytes(INDEX_TABLE2));
-    UTIL.deleteTable(Bytes.toBytes(indexedTableName));
+    closeAndCleanupTables(primary, index1, index2);
+  }
+
+  /**
+   * HBase has a 'fun' property wherein you can completely clobber an existing row if you make a
+   * {@link Put} at the exact same dimension (row, cf, cq, ts) as an existing row. The old row
+   * disappears and the new value (since the rest of the row is the same) completely subsumes it.
+   * This test ensures that we remove the old entry and put a new entry in its place.
+   * @throws Exception on failure
+   */
+  @Test
+  public void testOverwritingPutsCorrectlyGetIndexed() throws Exception {
+    // setup the index
+    CoveredColumnIndexSpecifierBuilder builder = new CoveredColumnIndexSpecifierBuilder();
+    builder.addIndexGroup(fam1);
+
+    // setup the primary table
+    String indexedTableName = "testMultipleTimestampsInSinglePut";
+    HTableDescriptor pTable = new HTableDescriptor(indexedTableName);
+    pTable.addFamily(new HColumnDescriptor(FAM));
+    pTable.addFamily(new HColumnDescriptor(FAM2));
+    builder.build(pTable);
+
+    // create the primary table
+    HBaseAdmin admin = UTIL.getHBaseAdmin();
+    admin.createTable(pTable);
+    HTable primary = new HTable(UTIL.getConfiguration(), indexedTableName);
+
+    // create the index tables
+    CoveredColumnIndexer.createIndexTable(admin, INDEX_TABLE);
+
+    // do a put to the primary table
+    byte[] row = Bytes.toBytes("row1");
+    byte[] value1 = Bytes.toBytes("val1");
+    byte[] value2 = Bytes.toBytes("val2");
+    byte[] value3 = Bytes.toBytes("val3");
+    Put p = new Put(row);
+    long ts = 10;
+    p.add(FAM, indexed_qualifer, ts, value1);
+    p.add(FAM, regular_qualifer, ts, value2);
+    primary.put(p);
+    primary.flushCommits();
+
+    // read the index for the expected values
+    HTable index1 = new HTable(UTIL.getConfiguration(), INDEX_TABLE);
+
+    // build the expected kvs
+    List<Pair<byte[], CoveredColumn>> pairs = new ArrayList<Pair<byte[], CoveredColumn>>();
+    pairs.add(new Pair<byte[], CoveredColumn>(value1, col1));
+    pairs.add(new Pair<byte[], CoveredColumn>(EMPTY_BYTES, col2));
+
+    // check the first entry at ts
+    List<KeyValue> expected = CoveredColumnIndexCodec.getIndexKeyValueForTesting(row, ts, pairs);
+    verifyIndexTableAtTimestamp(index1, expected, ts, value1);
+
+    // now overwrite the put in the primary table with a new value
+    p = new Put(row);
+    p.add(FAM, indexed_qualifer, ts, value3);
+    primary.put(p);
+    primary.flushCommits();
+
+    pairs = new ArrayList<Pair<byte[], CoveredColumn>>();
+    pairs.add(new Pair<byte[], CoveredColumn>(value3, col1));
+    pairs.add(new Pair<byte[], CoveredColumn>(EMPTY_BYTES, col2));
+
+    // check the first entry at ts
+    expected = CoveredColumnIndexCodec.getIndexKeyValueForTesting(row, ts, pairs);
+    verifyIndexTableAtTimestamp(index1, expected, ts, value3);
+    // and verify that a scan at the first entry returns nothing (ignore the updated row)
+    verifyIndexTableAtTimestamp(index1, Collections.<KeyValue> emptyList(), ts, value1, value2);
+
+    // cleanup
+    closeAndCleanupTables(primary, index1);
+  }
+  
+  @Test
+  public void testSimpleDelete() throws Exception{
+  //setup the index 
+    CoveredColumnIndexSpecifierBuilder builder = new CoveredColumnIndexSpecifierBuilder();
+    builder.addIndexGroup(fam1);
+
+    // setup the primary table
+    String indexedTableName = "testSimpleDelete";
+    HTableDescriptor pTable = new HTableDescriptor(indexedTableName);
+    pTable.addFamily(new HColumnDescriptor(FAM));
+    pTable.addFamily(new HColumnDescriptor(FAM2));
+    builder.build(pTable);
+
+    // create the primary table
+    HBaseAdmin admin = UTIL.getHBaseAdmin();
+    admin.createTable(pTable);
+    HTable primary = new HTable(UTIL.getConfiguration(), indexedTableName);
+
+    // create the index tables
+    CoveredColumnIndexer.createIndexTable(admin, INDEX_TABLE);
+
+    // do a delete on the primary table (no data, so no index updates...hopefully).
+    byte[] row1 = Bytes.toBytes("row1");
+    byte[] value1 = Bytes.toBytes("val1");
+    byte[] value2 = Bytes.toBytes("val2");
+    long ts = 10;
+    Delete d = new Delete(row1);
+    primary.delete(d);
+    primary.flushCommits();
+
+    HTable index1 = new HTable(UTIL.getConfiguration(), INDEX_TABLE);
+    List<KeyValue> expected = Collections.<KeyValue> emptyList();
+    verifyIndexTableAtTimestamp(index1, expected, ts, value1);
+
+    // a delete of a specific family/column should also not show any index updates
+    d = new Delete(row1);
+    d.deleteColumn(FAM, indexed_qualifer);
+    primary.delete(d);
+    primary.flushCommits();
+    verifyIndexTableAtTimestamp(index1, expected, ts, value1);
+
+    // also just a family marker should have the same effect
+    d = new Delete(row1);
+    d.deleteFamily(FAM);
+    primary.delete(d);
+    primary.flushCommits();
+    verifyIndexTableAtTimestamp(index1, expected, ts, value1);
+
+    // also just a family marker should have the same effect
+    d = new Delete(row1);
+    d.deleteColumns(FAM, indexed_qualifer);
+    primary.delete(d);
+    primary.flushCommits();
+    verifyIndexTableAtTimestamp(index1, expected, ts, value1);
+
+    // cleanup
+    closeAndCleanupTables(primary, index1);
   }
 
   private void verifyIndexTableAtTimestamp(HTable index1, List<KeyValue> expected, long ts,
       byte[] startKey) throws IOException {
-    Scan s = new Scan(startKey);
+    verifyIndexTableAtTimestamp(index1, expected, ts, startKey, HConstants.EMPTY_END_ROW);
+  }
+
+  private void verifyIndexTableAtTimestamp(HTable index1, List<KeyValue> expected, long ts,
+      byte[] startKey, byte[] endKey) throws IOException {
+    Scan s = new Scan(startKey, endKey);
     s.setTimeStamp(ts);
     // s.setRaw(true);
     List<KeyValue> received = new ArrayList<KeyValue>();
@@ -276,5 +393,16 @@ public class TestEndToEndCoveredIndexing {
     }
     scanner.close();
     assertEquals("Didn't get the expected kvs from the index table!", expected, received);
+  }
+
+  private void closeAndCleanupTables(HTable... tables) throws IOException {
+    if (tables == null) {
+      return;
+    }
+
+    for (HTable table : tables) {
+      table.close();
+      UTIL.deleteTable(table.getTableName());
+    }
   }
 }
